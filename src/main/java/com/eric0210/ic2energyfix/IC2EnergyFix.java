8e8dcdeb681bbf59a7c0966e4add4f7d5b1e3fc9
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2022-07-17 17:49:38 +0900

    Gradle build script fix  (maybe)
    still unstable

--- src/main/java/com/eric0210/ic2energyfix/IC2EnergyFix.java
+++ src/main/java/com/eric0210/ic2energyfix/IC2EnergyFix.java
@@ -1,49 +1,48 @@
 package com.eric0210.ic2energyfix;
 
 import java.util.Map;
-import java.util.logging.Logger;
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
-import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-
 public class IC2EnergyFix implements IFMLLoadingPlugin
 {
-	public IC2EnergyFix()
-	{
-		MixinBootstrap.init();
-		Mixins.addConfiguration("mixins.ic2energyfix.json");
-	}
+    public IC2EnergyFix()
+    {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.ic2energyfix.json");
+    }
 
-	@Override
-	public String[] getASMTransformerClass()
-	{
-		return ZERO_LENGTH_STRING_ARRAY;
-	}
+    @Override
+    public String[] getASMTransformerClass()
+    {
+        return ZERO_LENGTH_STRING_ARRAY;
+    }
 
-	@Override
-	public String getModContainerClass()
-	{
-		return null;
-	}
+    @Override
+    public String getModContainerClass()
+    {
+        return null;
+    }
 
-	@Override
-	public String getSetupClass()
-	{
-		return null;
-	}
+    @Override
+    public String getSetupClass()
+    {
+        return null;
+    }
 
-	@Override
-	public void injectData(final Map data)
-	{
-	}
+    @Override
+    public void injectData(final Map data)
+    {
+    }
 
-	@Override
-	public String getAccessTransformerClass()
-	{
-		return null;
-	}
+    @Override
+    public String getAccessTransformerClass()
+    {
+        return null;
+    }
 
-	private static final String[] ZERO_LENGTH_STRING_ARRAY = new String[0];
+    private static final String[] ZERO_LENGTH_STRING_ARRAY = new String[0];
 }
