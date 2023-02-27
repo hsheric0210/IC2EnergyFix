2c02a9d75c83889142ea6f1576acc7190d048f52
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-02-27 11:33:13 +0900

    LiquidHeatExchanger fixed

--- src/main/java/com/eric0210/ic2energyfix/mixins/ic2/block/tileentity/MixinTileEntityLiquidHeatExchanger.java
+++ src/main/java/com/eric0210/ic2energyfix/mixins/ic2/block/tileentity/MixinTileEntityLiquidHeatExchanger.java
@@ -2,11 +2,13 @@ package com.eric0210.ic2energyfix.mixins.ic2.block.tileentity;
 
 import com.eric0210.ic2energyfix.IC2EnergyFixConfig;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import ic2.core.block.invslot.InvSlotConsumable;
 import ic2.core.block.machine.tileentity.TileEntityLiquidHeatExchanger;
@@ -35,9 +37,25 @@ public class MixinTileEntityLiquidHeatExchanger
 		return outputTankCapacity;
 	}
 
-	@ModifyConstant(method = "getMaxHeatEmittedPerTick", constant = @Constant(intValue = 10), remap = false)
-	public int injectHeatPerExchangerFix(final int _10)
+	// Previous implementation; doesn't work because ModifyConstant doesn't supports IINC instruction
+	// @ModifyConstant(method = "getMaxHeatEmittedPerTick", constant = @Constant(intValue = 10), remap = false)
+	// public int injectHeatPerExchangerFix(final int _10)
+	// {
+	// return maxHeatExchangePerTick / heatexchangerslots.size();
+	// }
+
+	private int count = 0;
+
+	@Inject(method = "getMaxHeatEmittedPerTick", at = @At(value = "JUMP", opcode = Opcodes.IFNE, ordinal = 0, shift = Shift.AFTER), remap = false)
+	public void injectHeatPerExchangerFix1(final CallbackInfoReturnable<? super Integer> cir)
 	{
-		return maxHeatExchangePerTick / heatexchangerslots.size();
+		count += maxHeatExchangePerTick/10;
+	}
+
+	@Inject(method = "getMaxHeatEmittedPerTick", at = @At(value = "RETURN", shift = Shift.BEFORE), cancellable = true, remap = false)
+	public void injectHeatPerExchangerFix2(final CallbackInfoReturnable<? super Integer> cir)
+	{
+		cir.setReturnValue(count);
+		count = 0;
 	}
 }
